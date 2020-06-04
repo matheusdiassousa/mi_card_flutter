5d10c73a71229db846333206332cc26af4402ea8
Matheus <matheus_diassousa@hotmail.com>
2020-06-03 22:10:10 -0300

    lesson 6.4

--- lib/main.dart
+++ lib/main.dart
@@ -13,16 +13,38 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.fromLTRB(30.0, 10.0, 50.0, 20.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+        body: SafeArea( 
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                //width: 100.0,
+                margin: EdgeInsets.fromLTRB(10, 10.0, 10.0, 0.0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.white,
+                child: Text('Container 1'),
+              ),
+              Container(
+                height: 100.0,
+                //width: 100.0,
+                margin: EdgeInsets.fromLTRB(10.0, 10.0, 10, 0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.red,
+                child: Text('Container 2'),
+              ),
+              Container(
+                height: 100.0,
+                //width: 100.0,
+                margin: EdgeInsets.fromLTRB(10, 10.0, 10, 0),
+                padding: EdgeInsets.all(10.0),
+                color: Colors.green,
+                child: Text('Container 3', ),
+              ),
+                 
+            ],
           ),
-        ),
+       ),
       ),
     );
   }
