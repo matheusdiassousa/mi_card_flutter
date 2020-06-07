dfe59c193556ac4ce5fe6d9392a036aeab9dfbdc
Matheus <matheus_diassousa@hotmail.com>
2020-06-07 18:50:07 -0300

    mi_card_app finished using containers widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -13,29 +14,88 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: SafeArea( 
-          
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              CircleAvatar(
-                //child: Image("images/Perfil.png"),
-                backgroundImage: AssetImage("images/Perfil.png"),
-                radius: 50.0,
-                backgroundColor: Colors.white,
-              ),
-              Text(
-                'Matheus Dias Sousa',
-                style: TextStyle(
-                  fontSize: 21.0,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.white,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  //child: Image("images/Perfil.png"),
+                  backgroundImage: AssetImage("images/Perfil.png"),
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                ),
+                Text(
+                  'Matheus Dias',
+                  style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 35.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
-              ),
-                 
-            ],
+                ),
+                Text(
+                  'Microelectronics Design',
+                  style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 25.0,
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0),
+                  margin: EdgeInsets.symmetric(vertical: 3, horizontal: 25),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.email,
+                        size: 25,
+                        color: Colors.teal[900],
+                      ),
+                      SizedBox(width: 5,),
+                      Text(' matheus_diassousa@hotmail.com',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal.shade900,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.fromLTRB(10.0, 5.0, 10.0, 0),
+                  margin: EdgeInsets.symmetric(vertical: 3, horizontal: 25),
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Icon(Icons.phone,
+                        size: 25,
+                        color: Colors.teal[900],
+                      ),
+                      SizedBox(width: 5,),
+                      Text('+55 9 6053 9908',
+                        style: TextStyle(
+                          fontFamily: 'SourceSansPro',
+                          color: Colors.teal.shade900,
+                          fontSize: 16.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
-       ),
+        ),
       ),
     );
   }
